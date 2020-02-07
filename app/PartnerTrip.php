1ab99b0608ea22c4e622e6540e9a446eaf1b4f1c
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-02-07 18:39:14 +0200

    fix partner schema and soft deletes partners, partner_users, partner_trips, and partner_trip_stations

--- app/PartnerTrip.php
+++ app/PartnerTrip.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PartnerTrip extends Model
 {
+    use SoftDeletes;
+    
     protected $guarded = [];
 
     public function driver()
