3d5c20b652c701792f31c127967f2f7b02bf6f3f
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-03-14 16:58:02 +0200

    Rename trip start/end date column

--- database/migrations/2020_03_14_135830_rename_trip_start_end_date.php
+++ database/migrations/2020_03_14_135830_rename_trip_start_end_date.php
@@ -14,16 +14,8 @@ class RenameTripStartEndDate extends Migration
     public function up()
     {
         Schema::table('partner_trips', function (Blueprint $table) {
-            
-            if (env('DB_CONNECTION') === 'mysql'){
-                $table->renameColumn('startDate', 'start_date');
-                $table->renameColumn('endDate', 'end_date');
-            } else if (env('DB_CONNECTION') === 'pgsql'){
-                // PostgreSQL
-                DB::statement("ALTER TABLE partner_trips RENAME COLUMN startDate TO start_date");
-                DB::statement("ALTER TABLE partner_trips RENAME COLUMN endDate TO end_date");
-            }
-
+            $table->renameColumn('"startDate"', 'start_date');
+            $table->renameColumn('"endDate"', 'end_date');
         });
     }
 
@@ -35,8 +27,8 @@ class RenameTripStartEndDate extends Migration
     public function down()
     {
         Schema::table('partner_trips', function (Blueprint $table) {
-            $table->renameColumn('start_date', 'startDate');
-            $table->renameColumn('end_date', 'endDate');
+            $table->renameColumn('start_date', '"startDate"');
+            $table->renameColumn('end_date', '"endDate"');
         });
     }
 }
