a6e15d043c69f39f9b2562624c9f8358b1400cd8
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-03-14 15:31:37 +0200

    User and driver scheduled trips

--- database/migrations/2020_03_14_111344_add_file_id_to_roles_table.php
+++ database/migrations/2020_03_14_111344_add_file_id_to_roles_table.php
@@ -4,13 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRolesTable extends Migration
+class AddFileIdToRolesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('roles', function (Blueprint $table) {
