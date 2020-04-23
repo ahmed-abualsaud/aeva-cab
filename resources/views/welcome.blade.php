c1d06c21b8606a36844b301d76e6eb210caa38d0
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-04-23 14:44:19 +0200

    Welcome Page Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Qruz</title>
 
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
