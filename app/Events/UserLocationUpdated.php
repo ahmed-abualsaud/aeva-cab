5205cd7272d2a1403a3c43f3570151eb008aec1a
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-07-14 23:28:28 +0200

    User location updated event

---
+++ app/Events/UserLocationUpdated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserLocationUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $location;
+    protected $tripID;
+
+    public function __construct($location, $tripID)
+    {
+        $this->location = $location;
+        $this->tripID = $tripID;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel($this->tripID);
+    }
+}
