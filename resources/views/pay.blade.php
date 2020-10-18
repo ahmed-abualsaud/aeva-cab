2cc2fb0d01b17bddbe12d001c8f4d6ced597f31d
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-10-18 11:00:05 +0200

    Pay page css fixes

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -19,7 +19,7 @@
     </style>
     <style>
         body {
-            background-color: #edeff0 !important;
+            background-color: transparent !important;
         }
         .card, .alert {
             border-radius: 1rem !important;
@@ -86,48 +86,46 @@
         <!-- <p class="mb-0">Add money to wallet from credit card</p> -->
     </nav>
     <!-- CREATE THE HTML FOR THE PAYMENT PAGE -->
-    <div class="container mb-auto mt-4">
+    <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-md-4" id="card-content">
                 <div id="loader">
                     <div class='spinner spinner-dark'></div>
                     <p class="text-muted text-center" id="loaderStatus">Initializing..</p>
                 </div>
-                <div class="card border-0" id="cardForm">
-                    <div class="card-body">
-                        <div id="feedback"></div>
-                        <div class="form-row">
-                            <input type="hidden" id="token" value="{{ app('request')->input('token') }}">
-                            <div class="form-group col-12">
-                                <label for="cardNumber" class="mb-0 font-weight-bold">Card Number</label>
-                                <input type="number" id="cardNumber" class="form-control" value="" readonly  />
-                            </div>
-                            <div class="form-group col-4">
-                                <label for="cardMonth" class="mb-0 font-weight-bold">Month</label>
-                                <input type="number" id="cardMonth" class="form-control" placeholder="MM" value="" autocomplete="off" />
-                            </div>
-                            <div class="form-group col-4">
-                                <label for="cardYear" class="mb-0 font-weight-bold">Year</label>
-                                <input type="number" id="cardYear" class="form-control" placeholder="YYYY" value="" autocomplete="off" />
-                            </div>
-                            <div class="form-group col-4">
-                                <label for="cardCVC" class="mb-0 font-weight-bold">CVC</label>
-                                <input type="number" id="cardCVC" class="form-control" value="" readonly />
-                            </div>
-                            <div class="d-inline-flex font-weight-bold mb-4">
-                                <p class="mb-0 align-self-center">Add</p>
-                                <input type="number" placeholder="type amount" id="amount" class="form-control mx-2" autocomplete="off" style="width: 35%" />
-                                <p class="mb-0 align-self-center">EGP to my wallet</p>
-                            </div>
-                            <!-- <div class="form-group col-12">
-                                <label for="amount" class="mb-0 font-weight-bold">Amount</label>
-                                <input type="number" placeholder="EGP" id="amount" class="form-control" value="" autocomplete="off" />
-                            </div> -->
-                            <img src="{{ asset('assets/vapulus-checkout.png') }}"   
-                                class="img-fluid" id="payButton" onclick="pay();" 
-                                style="cursor: pointer;" 
-                            />
+                <div id="cardForm">
+                    <div id="feedback"></div>
+                    <div class="form-row">
+                        <input type="hidden" id="token" value="{{ app('request')->input('token') }}">
+                        <div class="form-group col-12">
+                            <label for="cardNumber" class="mb-0 font-weight-bold">Card Number</label>
+                            <input type="number" id="cardNumber" class="form-control" value="" readonly  />
                         </div>
+                        <div class="form-group col-4">
+                            <label for="cardMonth" class="mb-0 font-weight-bold">Month</label>
+                            <input type="number" id="cardMonth" class="form-control" placeholder="MM" value="" autocomplete="off" />
+                        </div>
+                        <div class="form-group col-4">
+                            <label for="cardYear" class="mb-0 font-weight-bold">Year</label>
+                            <input type="number" id="cardYear" class="form-control" placeholder="YYYY" value="" autocomplete="off" />
+                        </div>
+                        <div class="form-group col-4">
+                            <label for="cardCVC" class="mb-0 font-weight-bold">CVC</label>
+                            <input type="number" id="cardCVC" class="form-control" value="" readonly />
+                        </div>
+                        <div class="d-inline-flex font-weight-bold mb-4">
+                            <p class="mb-0 align-self-center">Add</p>
+                            <input type="number" placeholder="type amount" id="amount" class="form-control mx-2" autocomplete="off" style="width: 35%" />
+                            <p class="mb-0 align-self-center">EGP to my wallet</p>
+                        </div>
+                        <!-- <div class="form-group col-12">
+                            <label for="amount" class="mb-0 font-weight-bold">Amount</label>
+                            <input type="number" placeholder="EGP" id="amount" class="form-control" value="" autocomplete="off" />
+                        </div> -->
+                        <img src="{{ asset('assets/vapulus-checkout.png') }}"   
+                            class="img-fluid" id="payButton" onclick="pay();" 
+                            style="cursor: pointer;" 
+                        />
                     </div>
                 </div>
             </div>
