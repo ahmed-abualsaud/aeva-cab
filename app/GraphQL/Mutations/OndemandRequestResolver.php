7d6041e3a107babcbc60ce7de10f94eefe835721
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2021-01-26 09:23:20 +0200

    Qruz ondemand mutations refactoring

--- app/GraphQL/Mutations/OndemandRequestResolver.php
+++ app/GraphQL/Mutations/OndemandRequestResolver.php
@@ -2,105 +2,114 @@
 
 namespace App\GraphQL\Mutations;
 
-use App\User;
 use App\OndemandRequest;
 use App\Mail\DefaultMail;
 use App\OndemandRequestLine;
 use App\OndemandRequestVehicle;
 use App\Events\RequestSubmitted;
 use App\Jobs\SendPushNotification;
+use App\Traits\HandleDeviceTokens;
+use Illuminate\Support\Facades\DB;
 use App\Exceptions\CustomException;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class OndemandRequestResolver
 {
+    use HandleDeviceTokens;
     /**
      * @param  null  $_
      * @param  array<string, mixed>  $args
      */
     public function create($_, array $args)
     {
+        DB::beginTransaction();
         try {
             $input = collect($args)->except(['directive', 'vehicles', 'lines'])->toArray();
             $request = OndemandRequest::create($input);
- 
-            $vehicles_data = array(); 
-            $vehicles_arr = array();
-            foreach($args['vehicles'] as $vehicle) {
-                $vehicles_arr['request_id'] = $request->id;
-                $vehicles_arr['car_type_id'] = $vehicle['car_type_id'];
-                $vehicles_arr['car_model_id'] = $vehicle['car_model_id'];
-                $vehicles_arr['count'] = $vehicle['count'];
-                array_push($vehicles_data, $vehicles_arr);
-            } 
-            OndemandRequestVehicle::insert($vehicles_data);
+            $this->createVehicles($args['vehicles'], $request->id);
+            $this->createLines($args['lines'], $request->id);
 
-            $lines_data = array(); 
-            $lines_arr = array();
-            foreach($args['lines'] as $line) {
-                $lines_arr['request_id'] = $request->id;
-                $lines_arr['from_lat'] = $line['from_lat'];
-                $lines_arr['from_lng'] = $line['from_lng'];
-                $lines_arr['to_lat'] = $line['to_lat'];
-                $lines_arr['to_lng'] = $line['to_lng'];
-                $lines_arr['from_address'] = $line['from_address'];
-                $lines_arr['to_address'] = $line['to_address'];
-                array_push($lines_data, $lines_arr);
-            } 
-            OndemandRequestLine::insert($lines_data);
+            DB::commit();
         } catch (\Exception $e) {
+            DB::rollback();
             throw new CustomException('We could not able to create this request.' . $e->getMessage());
         }
 
         $this->broadcastRequest($request);
 
+        $this->mailRequest(
+            "New On-Demand request has been submitted", 
+            "Qruz On Demand", 
+            $request->id
+        );
+
         return $request;
+    }
+
+    protected function createVehicles($vehicles, $requestId)
+    {
+        $vehicles_arr = ['request_id' => $requestId];
+        foreach($vehicles as $vehicle) {
+            $vehicles_arr['car_type_id'] = $vehicle['car_type_id'];
+            $vehicles_arr['car_model_id'] = $vehicle['car_model_id'];
+            $vehicles_arr['count'] = $vehicle['count'];
+            $vehicles_data[] =  $vehicles_arr;
+        } 
+        OndemandRequestVehicle::insert($vehicles_data);
+    }
+
+    protected function createLines($lines, $requestId)
+    {
+        $lines_arr = ['request_id' => $requestId];
+        foreach($lines as $line) {
+            $lines_arr['from_lat'] = $line['from_lat'];
+            $lines_arr['from_lng'] = $line['from_lng'];
+            $lines_arr['to_lat'] = $line['to_lat'];
+            $lines_arr['to_lng'] = $line['to_lng'];
+            $lines_arr['from_address'] = $line['from_address'];
+            $lines_arr['to_address'] = $line['to_address'];
+            $lines_data[] = $lines_arr;
+        } 
+        OndemandRequestLine::insert($lines_data);
     }
 
     public function update($_, array $args)
     {
-        
-        $input = collect($args)->except(['id', 'directive'])->toArray();
         try {
+            $input = collect($args)->except(['id', 'directive'])->toArray();
             $request = OndemandRequest::findOrFail($args['id']);
         } catch (ModelNotFoundException $e) {
             throw new CustomException('The provided request ID is not found.');
         }
 
-        if (array_key_exists('status', $args) && $args['status']) {
-            
-            if ($request->status === 'CANCELLED' || $request->status === 'REJECTED') {
-                throw new CustomException('Request status can not be changed.');
-            }
-            
-            if ($args['status'] === 'CANCELLED' && $request->status !== 'PENDING') {
-                throw new CustomException('This request can not be cancelled.');
-            }
-
-            if ($args['status'] !== 'CANCELLED') {
-                $responseTitle = 'Your Ondemand request ID ' . $request->id . ' has been ' . strtolower($args['status']);
-                $responseMsg = $responseTitle;
-                if (array_key_exists('response', $args) && $args['response']) 
-                    $responseMsg .= '. '. $args['response'];
-                
-                $token = User::select('device_id')->find($request->user_id)->device_id;
-                if ($token)
-                    SendPushNotification::dispatch($token, $responseMsg);
-                
-            } else {
-                $title = "On-Demand Request Cancelled";
-                $message = "On-Demand request ID ". $request->id ." has been cancelled by user";
-                if (array_key_exists('comment', $args) && $args['comment'])
-                    $message .= ". ".$args['comment'];
-
-                $this->mailRequest($message, $title, $request->id);
-            }
-        }
-
+        if (array_key_exists('status', $args) && $args['status']) 
+            $this->updateStatus($request, $args);
+        
         $request->update($input);
 
         return $request;
+    }
+
+    protected function updateStatus($request, $args)
+    {
+        if ($request->status === 'CANCELLED' || $request->status === 'REJECTED')
+            throw new CustomException('Request status can not be changed.');
+        
+        if ($args['status'] === 'CANCELLED' && $request->status !== 'PENDING') 
+            throw new CustomException('This request can not be cancelled.');
+
+        $responseMsg = 'Your Ondemand request ID ' . $request->id . ' has been ' . strtolower($args['status']);
+
+        if (array_key_exists('response', $args) && $args['response']) 
+            $responseMsg .= '. '. $args['response'];
+        
+        SendPushNotification::dispatch(
+            $this->getUserToken($request->user_id), 
+            $responseMsg,
+            'Qruz On Demand'
+        ); 
+        
     }
 
     public function destroy($_, array $args)
@@ -110,11 +119,6 @@ class OndemandRequestResolver
 
     protected function broadcastRequest($request)
     {
-        $title = "New On-Demand Request";
-        $message = "New On-Demand request has been submitted!";
-
-        $this->mailRequest($message, $title, $request->id);
-
         $req = [
             'id' => $request->id,
             'verb' => $request->verb,
