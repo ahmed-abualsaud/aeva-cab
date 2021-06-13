6f555a4608c01ec84a759830fcece153bda16e62
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2021-06-13 15:41:06 +0200

    Remove is_direct attribute from schema

--- app/GraphQL/Queries/CommunicationResolver.php
+++ app/GraphQL/Queries/CommunicationResolver.php
@@ -14,14 +14,11 @@ class CommunicationResolver
     public function businessTripChatMessages($_, array $args)
     {
         try {
-            $is_private = (array_key_exists('is_direct', $args) && $args['is_direct'])
-                || (array_key_exists('is_private', $args) && $args['is_private']);
-
             $messages = BusinessTripChat::with('sender:id,name')
                 ->where('log_id', $args['log_id'])
                 ->selectRaw('business_trip_chat.*, DATE_FORMAT(created_at, "%l:%i %p") as time');
     
-            if ($is_private) {
+            if (array_key_exists('is_private', $args) && $args['is_private']) {
                 $messages = $messages->where('is_private', true)
                     ->where(function ($query) use ($args) {
                         $query->where('sender_id', $args['user_id'])
