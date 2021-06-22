56eac884df8a46cee0755aec2f34dbdf84b8aa65
sicoman <ahmedtheseer891996@gmail.com>
2021-06-22 13:04:24 -0400

    modified SeatsTripTerminalTransactionController.php

--- app/Http/Controllers/SeatsTripTerminalTransactionController.php
+++ app/Http/Controllers/SeatsTripTerminalTransactionController.php
@@ -4,33 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SeatsTripTerminalTransaction;
-use App\Partner;
 
 class SeatsTripTerminalTransactionController extends Controller
 {
     public function create(Request $req) {
 
-        $obj = json_decode(json_encode($req->obj));
+        try {
 
-        //return response()->json($obj);
-    	if(!($obj->pending) && !($obj->success)){
-    		$status = 'DECLINED';
-    	}
+            $obj = json_decode(json_encode($req->obj));
+    
+            $data = [
+                'trx_id' => $obj->id,
+                'partner_id' => $obj->profile_id,
+                'terminal_id' => $obj->terminal_id,
+                'source' => $obj->api_source,
+                'amount' => $obj->amount_cents/100,
+                'status' => $this->status($obj),
+                'created_at' => $obj->created_at,
+            ];
+    
+            return SeatsTripTerminalTransaction::create($data);
+            
+        } catch (\Exception $e) {
+            //
+        }
+    }
 
-    	else if(!($obj->pending) && ($obj->success)) {
-    		$status = 'SUCCESS';
-    	}
-
-    	else $status = 'PENDING';
-
-    	return SeatsTripTerminalTransaction::create([
-    		'trnx_id'     => $obj->id,
-    		'partner_id'  => $obj->profile_id,
-            'terminal_id' => $obj->terminal_id,
-    		'source'      => $obj->api_source,
-    		'amount'      => $obj->amount_cents,
-    		'status'      => $status,
-            'created_at'  => $obj->created_at,
-    	]);
+    protected function status($obj)
+    {
+        if(!($obj->pending) && !($obj->success))
+            return 'DECLINED';
+        
+        if (!($obj->pending) && ($obj->success))
+            return 'SUCCESS';
+        
+        return 'PENDING';
     }
 }
