564a8f4f788c1a2f60f021ca675df8f07829f24c
sicoman <ahmedtheseer891996@gmail.com>
2021-08-23 14:32:57 +0200

    business trip student absence handling

--- app/Repository/Eloquent/Mutations/BusinessTripAttendanceRepository.php
+++ app/Repository/Eloquent/Mutations/BusinessTripAttendanceRepository.php
@@ -6,32 +6,19 @@ use App\BusinessTripAttendance;
 use App\Traits\HandleDeviceTokens;
 use App\Exceptions\CustomException;
 use App\Traits\HandleBusinessTripUserStatus;
-use App\Repository\Eloquent\BaseRepository;
 
-class BusinessTripAttendanceRepository extends BaseRepository
+class BusinessTripAttendanceRepository
 {
     use HandleBusinessTripUserStatus;
     use HandleDeviceTokens;
 
-    public function __construct(BusinessTripAttendance $model)
-    {
-        parent::__construct($model);
-    }
-
     public function create(array $args)
     {
-        try {
+        try {            
             if ($args['date'] === date('Y-m-d'))
-                $this->updateUserStatus(
-                    $args['trip_id'], 
-                    ['is_absent' => $args['is_absent']], 
-                    $args['user_id']
-                );
-            
-            $firstArgs = collect($args)->only(['date', 'trip_id', 'user_id'])->toArray();
-            $secondArgs = collect($args)->only(['is_absent', 'comment'])->toArray();
-            
-           return $this->model->updateOrCreate($firstArgs, $secondArgs);
+                $this->updateUserStatusWithStudents($args);
+                
+            return $this->updateUserAttendance($args);
 
         } catch(\Exception $e) {
             throw new CustomException(__('lang.create_attendance_failed'));
--- app/Traits/HandleBusinessTripUserStatus.php
+++ app/Traits/HandleBusinessTripUserStatus.php
@@ -2,8 +2,10 @@
 
 namespace App\Traits;
 
+use App\BusinessTrip;
 use App\BusinessTripSubscription;
 use App\StudentSubscription;
+use App\BusinessTripAttendance;
 
 trait HandleBusinessTripUserStatus
 {
@@ -22,16 +24,67 @@ trait HandleBusinessTripUserStatus
         return $usersStatus->update($status);
     }
 
-    protected function updateUserStudentsStatus($trip_id, $user_id, $students, $status)
+    protected function updateUserStudentsStatus($trip_id, $status, $user_id, $students)
     {
-        $userStudents = StudentSubscription::where('trip_id', $trip_id)
-            ->where('user_id', $user_id);
+        $userStudents = $this->getUserStudents($trip_id, $user_id);
 
         $userStudents->whereIn('student_id', $students)->update($status);
 
-        if($userStudents->count() == count($students))
+        if($userStudents->count() == count($args['students']))
+            $this->updateUserStatus($trip_id, $status, $user_id);
+    }
+
+    protected function shouldUpdateUserAttendance(array $args)
+    {
+        $userStudents = $this->getUserStudents($args['trip_id'], $args['user_id']);
+
+        if($userStudents->count() == count($args['students']))
             return true;
 
         return false;
+    }
+
+    protected function updateUserAttendance(array $args)
+    {
+        if(BusinessTrip::findOrFail($trip_id)['type'] == 'TOSCHOOL' && array_key_exists('students', $args))
+        {
+            if($this->shouldUpdateUserAttendance($args)) 
+                return $this->updateAttendance($args);
+        }
+        else return $this->updateAttendance($args);      
+    }
+
+    protected function updateUserStatusWithStudents(array $args)
+    {
+        if(BusinessTrip::findOrFail($trip_id)['type'] == 'TOSCHOOL' && array_key_exists('students', $args))
+        {
+            $this->updateUserStudentsStatus(
+                $args['trip_id'], 
+                ['is_absent' => $args['is_absent']], 
+                $args['user_id'],
+                $args['students']
+            );
+        }
+        else {
+            $this->updateUserStatus(
+                $args['trip_id'], 
+                ['is_absent' => $args['is_absent']], 
+                $args['user_id']
+            );
+        }      
+    }
+
+    protected function getUserStudents($trip_id, $user_id)
+    {
+        return StudentSubscription::where('trip_id', $trip_id)
+            ->where('user_id', $user_id);
+    }
+
+    protected function updateAttendance(array $args)
+    {
+        $firstArgs = collect($args)->only(['date', 'trip_id', 'user_id'])->toArray();
+        $secondArgs = collect($args)->only(['is_absent', 'comment'])->toArray();
+        
+        return BusinessTripAttendance::updateOrCreate($firstArgs, $secondArgs);
     }
 }
