c8765fd5ab9e2011d49c84e4f3b314c264a4e386
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2021-12-22 00:22:34 +0200

    restore socket default values

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,14 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST'),
-                'port' => env('PUSHER_PORT'),
-                'scheme' => env('PUSHER_SCHEME'),
                 'useTLS' => true,
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
