2705447ecd4aa2c8c3ee072599fef9417c4e8f74
sicoman <ahmedtheseer891996@gmail.com>
2021-12-29 19:13:42 +0200

    add cab basic queries

--- app/CabRequest.php
+++ app/CabRequest.php
@@ -2,10 +2,15 @@
 
 namespace App;
 
+use App\Traits\Filterable;
+use App\Traits\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class CabRequest extends Model
 {
+    use Filterable;
+    use Searchable;
+
     protected $guarded = [];
 
     protected $casts = [
@@ -27,9 +32,38 @@ class CabRequest extends Model
         return $this->belongsTo(Vehicle::class);
     }
 
+    public function scopeLive($query)
+    {
+        return $query->where('status', 'STARTED');
+    }
+
     public function scopeWherePending($query, $user_id)
     {
         return $query->where('user_id', $user_id)
             ->whereNotIn('status' , ['CANCELLED', 'COMPLETED']);
+    }
+
+    public function scopeSearch($query, $args) 
+    {
+        
+        if (array_key_exists('searchQuery', $args) && $args['searchQuery']) {
+            $query = $this->search($args['searchFor'], $args['searchQuery'], $query);
+        }
+
+        return $query;
+    }
+
+    public function scopeFilter($query, $args) 
+    {
+        
+        if (array_key_exists('status', $args) && $args['status']) {
+            $query = $query->where('status', $args['status']);
+        }
+
+        if (array_key_exists('period', $args) && $args['period']) {
+            $query = $this->dateFilter($args['period'], $query, 'created_at');
+        }
+
+        return $query->latest();
     }
 }
--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,13 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                'host' => '127.0.0.1',
+                'host' => 'ws.qruz.xyz',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
