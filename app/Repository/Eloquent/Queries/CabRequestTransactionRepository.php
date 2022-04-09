d511505c68be850a2f3c966d1738e28da0acde1f
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2022-04-09 17:39:20 +0200

    Cab requests transactions stats

--- app/Repository/Eloquent/Queries/CabRequestTransactionRepository.php
+++ app/Repository/Eloquent/Queries/CabRequestTransactionRepository.php
@@ -4,26 +4,42 @@ namespace App\Repository\Eloquent\Queries;
 
 use App\CabRequestTransaction;
 use App\Repository\Eloquent\BaseRepository;
-
-use Illuminate\Support\Arr;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Traits\Filterable;
 
 
 class CabRequestTransactionRepository extends BaseRepository
 {
+    use Filterable;
+    
     public function __construct(CabRequestTransaction $model)
     {
         parent::__construct($model);
     }
 
-    public function report($args) 
+    public function stats(array $args)
     {
-        $report = $this->model->selectRaw('
-            ROUND(SUM(amount), 2) as sum,
-            COUNT(id) as count
-        ')
-        ->where('created_at', '>=', $args['date_from'])
-        ->where('created_at', '<=', $args['date_to']);
-        return $report->orderBy('sum', 'desc')->get();
+        $transactions = $this->model->query();
+
+        $transactionGroup = $this->model->selectRaw('
+            DATE_FORMAT(created_at, "%d %b %Y") as date,
+            ROUND(SUM(amount), 2) as sum
+        ');
+
+        if (array_key_exists('period', $args) && $args['period']) {
+            $transactions = $this->dateFilter($args['period'], $transactions, 'created_at');
+            $transactionGroup = $this->dateFilter($args['period'], $transactionGroup, 'created_at');
+        }
+
+        $transactionCount = $transactions->count();
+        $transactionSum = $transactions->sum('amount');
+        $transactionGroup = $transactionGroup->groupBy('date')->get();
+
+        $response = [
+            'count' => $transactionCount,
+            'sum' => round($transactionSum, 2),
+            'transactions' => $transactionGroup
+        ];
+
+        return $response;
     }
 }
