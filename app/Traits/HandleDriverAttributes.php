ce512fd091796d2c7aa9c6897953bbb30cdec0eb
sicoman <ahmedtheseer891996@gmail.com>
2022-06-03 16:45:12 +0200

    calculate total working time for the driver

--- app/Traits/HandleDriverAttributes.php
+++ app/Traits/HandleDriverAttributes.php
@@ -3,11 +3,42 @@
 namespace App\Traits;
 
 use App\Driver;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 trait HandleDriverAttributes
 {
-    protected function updateDriverStatus($driver_id, $status)
+    protected function updateDriverStatus($driver_id, $cab_status)
     {
-       return Driver::where('id', $driver_id)->update(['cab_status' => $status]);
+        try {
+            $driver = Driver::findOrFail($driver_id);
+        } catch (ModelNotFoundException $e) {
+            throw new \Exception(__('lang.request_not_found'));
+        }
+
+        if (strtolower($cab_status) == 'riding') {
+            return $driver->update([
+                'cab_status' => $cab_status
+            ]);
+        }
+
+        $activity_updated_at = date('Y-m-d H:i:s');
+
+        if (strtolower($cab_status) == 'offline' && $driver->cab_status == 'Online') {
+            $total_working_time = strtotime($activity_updated_at) - strtotime($driver->activity_updated_at);
+            $total_working_time = $total_working_time / 60 + $driver->total_working_time;
+            return $driver->update([
+                'cab_status' => $cab_status,
+                'total_working_time' => $total_working_time,
+                'activity_updated_at'=> $activity_updated_at
+            ]);
+        }
+
+        if (strtolower($cab_status) == 'online') {
+                return $driver->update([
+                'cab_status' => $cab_status,
+                'activity_updated_at'=> $activity_updated_at
+            ]);
+        }
     }
 
     protected function driversToken($drivers_ids)
--- database/migrations/2020_01_31_073756_create_drivers_table.php
+++ database/migrations/2020_01_31_073756_create_drivers_table.php
@@ -45,6 +45,8 @@ class CreateDriversTable extends Migration
             $table->bigInteger('received_cab_requests')->default(0);
             $table->bigInteger('accepted_cab_requests')->default(0);
             $table->bigInteger('cancelled_cab_requests')->default(0);
+            $table->bigInteger('total_working_time')->default(0);
+            $table->timestamp('activity_updated_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
