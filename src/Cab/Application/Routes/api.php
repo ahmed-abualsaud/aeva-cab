42b7454604feb859fb00aca7323e5ac493d5ad81
sicoman <ahmedtheseer891996@gmail.com>
2022-07-04 17:52:56 +0200

    fixing bugs

--- src/Cab/Application/Routes/api.php
+++ src/Cab/Application/Routes/api.php
@@ -9,7 +9,7 @@
 Route::group([
         'prefix' => 'rest',
         'middleware' => ['api', 'auth:driver'], 
-        'namespace' => 'Aeva\Cab\Application\HTTP\Controllers\Queries'
+        'namespace' => 'Aeva\Cab\Application\HTTP\Controllers\Mutations'
     ], function () {
         Route::post('/confirm/cashout', 'CabRequestTransactionController@confirmCashout');
 });
