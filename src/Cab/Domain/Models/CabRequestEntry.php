522ba32decce05ac294b1788b560e9857e5ee5b7
sicoman <ahmedtheseer891996@gmail.com>
2022-07-14 15:17:47 +0200

    update driver location & create cab entries using  websocket

--- src/Cab/Domain/Models/CabRequestEntry.php
+++ src/Cab/Domain/Models/CabRequestEntry.php
@@ -12,20 +12,6 @@ class CabRequestEntry extends Model
 
     public $timestamps = false;
 
-    public static function calculateDistance($args)
-    {
-        $last_location = self::getLastLocation($args['request_id']);
-        if($last_location) {
-            return (self::sphereDistance(
-                $args['latitude'], 
-                $args['longitude'], 
-                $last_location->latitude, 
-                $last_location->longitude
-            ) + $last_location->distance);
-        }
-        return 0;
-    }
-
     public static function getLastLocation($request_id)
     {
         return  self::where('request_id', $request_id)->latest()->first();
@@ -45,13 +31,5 @@ class CabRequestEntry extends Model
             return ResizableMapUrl::generatePath(implode('|', $path));
         }
         return null;
-    }
-
-    protected static function sphereDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo)
-    {
-        $rad = M_PI / 180;
-        $theta = $longitudeFrom - $longitudeTo;
-        $dist = sin($latitudeFrom * $rad) * sin($latitudeTo * $rad) +  cos($latitudeFrom * $rad) * cos($latitudeTo * $rad) * cos($theta * $rad);
-        return acos($dist) / $rad * 60 * 1853;
     }
 }
