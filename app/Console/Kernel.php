7942a71b729d2af92c29b90d9f54dc0db42cf1af
sicoman <ahmedtheseer891996@gmail.com>
2022-07-14 15:19:47 +0200

    add websocket to get all drivers locations

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,11 @@
 
 namespace App\Console;
 
+use App\Driver;
+use App\Events\AllDriversLocations;
+
+use Carbon\Carbon;
+
 use Illuminate\Support\Stringable;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Console\Scheduling\Schedule;
@@ -26,12 +31,28 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('database:backup')->cron('0 0 */3 * *')->at('01:00')
-            ->onSuccess(function (Stringable $output) {
-                Log::info('Database backup executed successfully!  output: '.$output);
+        // $schedule->command('database:backup')
+        //     ->cron('0 0 */3 * *')
+        //     ->at('01:00')
+        //     ->onSuccess(function (Stringable $output) {
+        //         Log::info('Database backup executed successfully!  output: '.$output);
+        //     })
+        //     ->onFailure(function (Stringable $output) {
+        //         Log::error('Database backup failed with error: '.$output);
+        //     });
+
+        $schedule->call(function () {
+                $dt = Carbon::now();
+                $x=60/10;
+                do{
+                    $locations = Driver::select('latitude', 'longitude')->get()->toArray();
+                    broadcast(new AllDriversLocations($locations));
+                    time_sleep_until($dt->addSeconds(10)->timestamp);
+                } while($x-- > 1);
             })
+            ->everyMinute()
             ->onFailure(function (Stringable $output) {
-                Log::error('Database backup failed with error: '.$output);
+                Log::error('Broadcast all drivers locations failed with error: '.$output);
             });
     }
 
