dda185c34fffc391443afbb6549dcf6c44bb9621
abduallah-zaghloul <abduallah.zaghloul@creditgo.app>
2022-08-18 16:11:24 +0200

    total working hours

---
+++ database/migrations/2022_06_13_162719_add_total_working_hours_to_driver_stats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class addTotalWorkingHoursToDriverStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('driver_stats', function (Blueprint $table) {
+            $table->unsignedDecimal('total_working_hours',12,2)->storedAs('total_working_time/60');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('driver_stats', function (Blueprint $table) {
+            $table->dropColumn('total_working_hours');
+        });
+    }
+}
