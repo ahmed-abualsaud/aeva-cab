347cd2af6a91f7873a3bed1102f025d99519d6da
sicoman <ahmedtheseer891996@gmail.com>
2022-10-17 11:36:13 +0200

    get user promocode usages for each promocode applied on a completed ride

--- app/PromoCodeUsage.php
+++ app/PromoCodeUsage.php
@@ -17,4 +17,23 @@ class PromoCodeUsage extends Model
         return $this->belongsTo(PromoCode::class, 'promo_code_id');
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeUsage($query, $args)
+    {
+        return $query->selectRaw('
+            promo_code_usages.user_id,
+            promo_codes.id as promo_code_id,
+            promo_codes.name as promo_code_name,
+            COUNT(promo_codes.id) as promo_code_count
+        ')
+        ->join('promo_codes', 'promo_code_usages.promo_code_id', 'promo_codes.id')
+        ->join('cab_requests', 'promo_code_usages.promo_code_id', 'cab_requests.promo_code_id')
+        ->where('status', 'Completed')
+        ->where('promo_code_usages.used', true)
+        ->groupBy('promo_codes.id', 'promo_code_usages.user_id');
+    }
 }
